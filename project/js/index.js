613a78a58ce71dc44417c8d6821ec4f87efe7880
Kaifur Rahman <rahmankaifur8@gmail.com>
2022-10-25 11:03:24 +0530

    home section imporved countdown added

--- project/js/index.js
+++ project/js/index.js
@@ -1,7 +1,32 @@
-today=new Date();
-//put month-1 of actual month
-var event=new Date(today.getFullYear()+1, 02, 24);
+//for time countdown
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 14, 2023 15:37:25").getTime();
 
-var one_day=1000*60*60*24;
-console.log(Math.ceil((event.getTime()-today.getTime())/(one_day))+
-" days left");
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("mins").innerHTML = minutes;
+  document.getElementById("secs").innerHTML = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countDown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+//for time countdown/
