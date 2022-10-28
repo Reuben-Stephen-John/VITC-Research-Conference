36b2cb7bffd68728ff1852b0f9c1c813871d1385
Kaifur Rahman <rahmankaifur8@gmail.com>
2022-10-28 14:16:13 +0530

    final improvement version-1

--- project/js/index.js
+++ project/js/index.js
@@ -42,8 +42,8 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
+    document.body.scrollTop > 450 ||
+    document.documentElement.scrollTop > 450
   ) {
     mybutton.style.display = "block";
   } else {
